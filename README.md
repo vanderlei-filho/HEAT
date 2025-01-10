e2d39a04222bc7adc01bee94bccd209863f360dd
Vanderlei <vanderlei.filho@proton.me>
2025-01-10 04:47:45 -0300

    Simplify Code

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# HEAT / Jacobi Method
+# HEAT2
 
 This is a parallel MPI implementation of the iterative [Jacobi method](https://en.wikipedia.org/wiki/Jacobi_method) for solving linear systems of equations.
 
 To compile this application you'll need an MPI distribution with ULFM support enabled.
+
+## Dependencies
+
+- MPI
+- libpng
+- CMake
+- Make
+
+## Building the Project
+
+```shell
+mkdir build && cd build && cmake .. && make
+```
+
--- src/main.c
+++ src/main.c
@@ -3,85 +3,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
 char **gargv = NULL;
 
-/**
- * Generates a border array of random values in the range of -0.5 to 0.5.
- *
- * @param border    Pointer to the array where border values will be stored.
- * @param nb_elems  Number of elements in the border array.
- * @return          Returns 0 on successful generation, -1 if the input pointer
- * is NULL.
- */
-int generate_border(TYPE *border, size_t nb_elems) {
-  if (!border) {
-    return -1;
-  }
-  for (size_t i = 0; i < nb_elems; i++) {
-    border[i] = 100.0;
+int generate_border(TYPE *border, int nb_elems) {
+  for (int i = 0; i < nb_elems; i++) {
+    border[i] = (TYPE)(((double)rand()) / ((double)RAND_MAX) - 0.5);
   }
   return 0;
 }
 
-/**
- * Initializes a matrix with a border and zeros inside.
- *
- * @param matrix  Pointer to the matrix to be initialized.
- * @param border  Pointer to the border array.
- * @param nb      Number of columns in the matrix (excluding border).
- * @param mb      Number of rows in the matrix (excluding border).
- * @return        Returns 0 on successful initialization.
- */
 int init_matrix(TYPE *matrix, const TYPE *border, int nb, int mb) {
   int i, j, idx = 0;
 
-  // Initialize the top border
-  for (idx = 0; idx < nb + 2; idx++) {
+  for (idx = 0; idx < nb + 2; idx++)
     matrix[idx] = border[idx];
-  }
   matrix += idx;
 
-  // Initialize the middle part of the matrix, including left and right borders
   for (j = 0; j < mb; j++) {
-    // Set left border value
     matrix[0] = border[idx];
     idx++;
-
-    // Set inner values to zero
-    for (i = 0; i < nb; i++) {
+    for (i = 0; i < nb; i++)
       matrix[1 + i] = 0.0;
-    }
-
-    // Set right border value
     matrix[nb + 1] = border[idx];
     idx++;
-
-    // Move to the next row
     matrix += (nb + 2);
   }
 
-  // Initialize the bottom border
-  for (i = 0; i < nb + 2; i++) {
+  for (i = 0; i < nb + 2; i++)
     matrix[i] = border[idx + i];
-  }
-
   return 0;
 }
 
-/**
- * The main function for the Jacobi solver program.
- */
 int main(int argc, char *argv[]) {
-  int i, rc, size, rank, NB = -1, MB = -1, P = -1, Q = -1, save_output = 0,
-                         max_iter = -1;
-  TYPE *om, *som, *border, epsilon = 1e-6;
+  int max_iter = 100;
+  int save_output = 0;
+  int i, rc, size, rank, NB = -1, MB = -1, P = -1, Q = -1;
+  TYPE *om, *som, *border, epsilon = 0;
   MPI_Comm parent;
 
   gargv = argv;
-  // Parse command-line arguments
+  /* get the problem size from the command arguments */
   for (i = 1; i < argc; i++) {
     if (!strcmp(argv[i], "-p")) {
       i++;
@@ -113,8 +76,6 @@ int main(int argc, char *argv[]) {
       continue;
     }
   }
-
-  // Check if required arguments are provided
   if (P < 1) {
     printf("Missing number of processes per row (-p #)\n");
     exit(-1);
@@ -130,31 +91,26 @@ int main(int argc, char *argv[]) {
   if (MB == -1) {
     MB = NB;
   }
-  if (max_iter == -1) {
-    printf("Missing the maximum number of iterations (-max_iter #)\n");
-    exit(-1);
-  }
 
-  if (rank == 0) {
-    printf(
-        "P = %d, Q = %d, NB = %d, MB = %d, max_iter = %d, save_output = %d\n",
-        P, Q, NB, MB, max_iter, save_output);
-  }
-
-  // Initialize the Jacobi CPU
   preinit_jacobi_cpu();
 
-  // Initialize MPI
   MPI_Init(NULL, NULL);
 
-  // Get the parent communicator
   MPI_Comm_get_parent(&parent);
   if (MPI_COMM_NULL == parent) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   }
+  /**
+   * Ugly hack to allow us to attach with a ssh-based debugger to the
+   * application.
+   */
+  int do_sleep = 0;
+  while (do_sleep) {
+    sleep(1);
+  }
 
-  // Initialize the border, matrix, and intermediate matrix
+  /* make sure we have some randomness */
   border = (TYPE *)malloc(sizeof(TYPE) * 2 * (NB + 2 + MB));
   om = (TYPE *)malloc(sizeof(TYPE) * (NB + 2) * (MB + 2));
   som = (TYPE *)malloc(sizeof(TYPE) * (NB + 2) * (MB + 2));
@@ -165,19 +121,17 @@ int main(int argc, char *argv[]) {
     init_matrix(om, border, NB, MB);
   }
 
-  // Set the error handler for MPI
   MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
 
-  // Run the Jacobi CPU solver
-  rc =
-      jacobi_cpu(om, NB, MB, P, Q, MPI_COMM_WORLD, 1e-6, max_iter, save_output);
+  rc = jacobi_cpu(om, NB, MB, P, Q, MPI_COMM_WORLD, epsilon, max_iter,
+                  save_output);
   if (rc < 0) {
     printf("The CPU Jacobi failed\n");
     goto cleanup_and_be_gone;
   }
 
 cleanup_and_be_gone:
-  // Free resources and finalize MPI
+  /* free the resources and shutdown */
   free(om);
   free(som);
   free(border);
